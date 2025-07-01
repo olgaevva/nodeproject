1b41d97bc7b22fe37687b87ddeec1b0b9f949a29
evva <olgaevva.itholic@gmail.com>
2025-07-01 15:11:41 +0300

    commonjs variant

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const os = require('os');
+let res1 = os.platform();
+const { getProducts } = require('./app');
+console.log(res1);
+/*function addNumber(num1, num2) {
+    let res = num1 + num2;
+    console.log(res);
+}*/
 
-function addNumber(num1, num2) {
-    let result = num1 + num2;
-    console.log(result);
-}
-
-addNumber(5, 3);
+console.log(getProducts());
+console.log('End');
