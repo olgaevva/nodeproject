eee003dd361f2b70ac30ea4227b0861e2b0da0f9
evva <olgaevva.itholic@gmail.com>
2025-07-04 18:23:33 +0300

    first variant with express

--- index.js
+++ index.js
@@ -1,59 +1,16 @@
-const http = require('http');
+// подключение express
+const express = require("express");
 const path = require('path');
-const fs = require('fs');
+const port = 8080
 
-http.createServer(function (req, res) {
-    console.log('Запрос по адресу:', req.url);
-    if (req.url === '/') {
-        const filePath = path.join(__dirname, 'index.html');
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Ошибка при загрузке файла');
-                return;
-            }
-
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Страница не найдена');
-    }
-}).listen(8080, () => {
-    console.log('Сервер запущен на http://localhost:8080');
+// создаем объект приложения
+const app = express();
+// определяем обработчик для маршрута "/"
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", function(request, response){
+    response.sendFile(path.join(__dirname,'public','index.html'));
 });
-
-
-/*function errorHandling(err) {
-    if (err) {
-        return console.error('Oooops '+ err);
-    }
-}*/
-/*let result = fs.readFileSync('./some.txt', 'utf8');
-console.log(result);
-fs.writeFileSync('./some.txt', result +'\nExample test')*/
-
-/*
-fs.mkdir('./images' , { recursive: true }, (err) => {
-    if (err) {
-        return console.error(err);
-    }
-    console.log('Directory created successfully!');
-    fs.writeFile('./images/image.txt', 'Hello', (err) => {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('File created successfully!');
-    })
-});
-*/
-
-/*fs.unlink('./images/image.txt', (err) => {
-    if (err) {
-        return console.error(err);
-    }
-});*/
-/*
-fs.rm('./images', { recursive: true },errorHandling);
-*/
+// начинаем прослушивать подключения на 3000 порту
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
