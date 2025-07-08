bc7cc6a9f4ff1883dee3f4df2d9bf7b8d750c0f1
evva <olgaevva.itholic@gmail.com>
2025-07-08 19:20:50 +0300

    jokes api

--- public/app.js
+++ public/app.js
@@ -1,15 +1,50 @@
 'use strict';
-
-async function getProducts() {
-	// fetch('https://dummyjson.com/products')
-	// 	.then(response => response.json())
-	// 	.then(data => console.log(data))
-	const productsResponse = await fetch('https://dummyjson.com/products');
-	const { products } = await productsResponse.json();
-	console.log(products);
-
-	const productResponse = await fetch('https://dummyjson.com/products/' + products[0].id);
-	const product = await productResponse.json();
-	console.log(product);
+function getRandomCategory (array) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    console.log(array[randomIndex])
+    return array[randomIndex];
 }
-module.exports = {getProducts};
+async function getJoke() {
+    try {
+        const categoryArray = ['Programming', 'Dark', 'Spooky', 'Pun', 'Christmas'];
+        const randomCategory = getRandomCategory(categoryArray);
+        const response = await fetch('https://v2.jokeapi.dev/joke/'+ randomCategory);
+        return await response.json()
+    } catch(e) {
+        console.error(e);
+        throw e;
+    }
+}
+async function main() {
+    const jokes = await Promise.all([
+        getJoke(),
+        getJoke(),
+        getJoke(),
+    ]);
+    console.log(jokes);
+    jokes.map((el, index) => {
+            createCards(el, index+1);
+        }
+    )
+}
+
+function createCards(joke, cardNumber) {
+    const divForCards = document.querySelector(`#card${cardNumber}`);
+    let jokeText = '';
+    if (joke.type === "twopart") {
+        jokeText = `${joke.setup} ${joke.delivery}`
+    } else {
+        jokeText = `${joke.joke}`
+    }
+    divForCards.innerHTML = `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="category-title">Category "${joke.category}"</h5>
+                <p class="card-text">${jokeText}</p>
+        </div>
+    </div>`;
+}
+
+let button = document.querySelector(".btn")
+button.addEventListener("click", async () => {
+    await main();
+})
